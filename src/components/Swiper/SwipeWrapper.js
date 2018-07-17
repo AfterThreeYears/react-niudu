f67e2bdb97e37c18720ad9264d8bee802e08ba26
shell <470326964@qq.com>
2018-07-17 11:39:18 +0800

    updaye

--- src/components/Swiper/SwipeWrapper.js
+++ src/components/Swiper/SwipeWrapper.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '@/components/Swiper/SwipeWrapper.css';
-import { getProperties } from '@/utils/styles';
-import { env } from '/Users/shell/wbb/git/github/react-niudu/src/utils/ua';
+import { getProperties, getPropNumeric } from '@/utils/styles';
+import { env } from '@/utils/ua';
 
 const ua = env();
 const properties = getProperties();
@@ -15,23 +15,66 @@ class SwipeWrapper extends Component {
     }
   }
   componentDidMount() {
-    console.log(this.swipeContainer.offsetWidth);
+    this.init();
+  }
+  init() {
+    // 获取swipeWrapper的宽度
+    const swiperWrapWidth = getPropNumeric(this.swiperWrap, 'width');
+    // 获取展示给用户的swipe宽度
+    const swipeContainerWidth = getPropNumeric(this.swipeContainer, 'width');
+    console.log(swipeContainerWidth);
+    // 最大可以滑动的值
+    const maxTransformX = swiperWrapWidth - swipeContainerWidth;
+    const noNeedTouchEvent = maxTransformX >= 0;
+    this.setState({
+      swiperWrapWidth,
+      swipeContainerWidth,
+      maxTransformX,
+      noNeedTouchEvent,
+    });
   }
   handleTouchStart = (e) => {
+    const { noNeedTouchEvent } = this.state;
+    if (noNeedTouchEvent) return;
     this.removeTransition();
-    const { x, y } = this.getTouchPosition(e);
-    this.prevTouchX = x;
+    const { x } = this.getTouchPosition(e);
+    this.setState({
+      startTime: Date.now(),
+      startX: this.state.transformX,
+      // 手指按下的x
+      prevTouchX: x,
+    });
   }
   handleTouchMove = (e) => {
-    const { x, y } = this.getTouchPosition(e);
-    this.startScroll = false;
     e.preventDefault();
-    let swipeX = this.state.transformX + x - this.prevTouchX;
+    const { x } = this.getTouchPosition(e);
+    let swipeX = this.state.transformX + x - this.state.prevTouchX;
     this.setTransformX(swipeX);
-    this.prevTouchX = x;
+    this.setState({
+      // 每次移动后需要把当前移动到的x赋值给prevTouchX，否则（x - this.prevTouchX）越来越大，移动也越来越快
+      prevTouchX: x,
+    })
   }
   handleTouchEnd = (e) => {
     this.addTransition();
+    this.resetTransition();
+    this.startAutoScroll();
+  }
+  startAutoScroll = () => {
+    const deltaT = Date.now() - this.state.startTime;
+    const deltaX = this.state.transformX - this.state.startX;
+    const v = deltaX / deltaT;
+    // 速度一定时或拉到尽头时不滚动
+    if (Math.abs(v) < 0.4 || deltaX === 0) {
+      return;
+    }
+    let transformPosition = deltaX + this.state.transformX;
+    if (deltaX > 0) {
+      transformPosition = Math.min(0, transformPosition);
+    } else {
+      transformPosition = Math.max(this.state.maxTransformX, transformPosition);
+    }
+    this.setTransformX(transformPosition);
   }
   getTouchPosition = (e) => {
     return {
@@ -53,9 +96,18 @@ class SwipeWrapper extends Component {
     const syncStyle = `${properties.transform} .4s ${ua.android ? 'cubic-bezier(0.04, 0.74, 0.36, 1)' : ''}`;
     this.swipeContainer.style[properties.transition] = syncStyle;
   }
+  resetTransition = () => {
+    const { transformX, maxTransformX } = this.state;
+    console.log(`transformX is ${transformX}, maxTransformX is ${maxTransformX}`);
+    if (transformX > 0) {
+      this.setTransformX(0);
+    } else if (maxTransformX > transformX) {
+      this.setTransformX(maxTransformX);
+    }
+  }
   render() {
     return (
-      <div className={'swiper-wrap'}>
+      <div className={'swiper-wrap'} ref={ref => this.swiperWrap = ref}>
         <div className={'swipe-overflow'} >
           <div className={'swiper-container'}
             ref={ref => this.swipeContainer = ref}
--- src/utils/styles.js
+++ src/utils/styles.js
@@ -7,4 +7,13 @@ export const getProperties = () => {
   };
 };
 
-export const letPxGo = value => Number(value.replace('px', ''));
+export const letPxGo = value => Number((value || '').replace('px', ''));
+export const getComputedProp = (el, prop) => (el instanceof Element ?
+  window.getComputedStyle(el, null)[prop] : null);
+export const getPropNumeric = (el, prop) => {
+  if (!el) {
+    return 0;
+  }
+  const value = letPxGo(getComputedProp(el, prop));
+  return isNaN(value) ? 0 : value;
+};
